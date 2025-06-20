cdc21b4cdfc1ae43403ca065b4c84344daf989ee
Roti18 <akuronif@gmail.com>
2025-06-20 19:29:34 +0700

    fix: bug deploy

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,6 @@ const nextConfig: NextConfig = {
   output: "export",
   assetPrefix: assetPrefix,
   basePath: basePath,
-
   images: {
     unoptimized: true,
     domains: ["storageangkatan.netlify.app"],
